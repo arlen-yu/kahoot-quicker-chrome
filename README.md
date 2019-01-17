391597f855e640a9e50e8bd1ccd1ae41cd4bc808
Arlen Yu <arlen.yu6@gmail.com>
2019-01-17 12:45:11 -0500

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Kahoot Quicker
+
+This chrome extension was created because I kept losing in kahoot in `KIN146`. The objective of this extension is to allow me to get first in the class of 400 people by using keybindings to select options much quicker. Wish me luck.
---
+++ js/kahoot.js
@@ -0,0 +1,39 @@
+function build_button_map(all_buttons) {
+  const colors_to_button = {}
+  for (i = 0; i < all_buttons.length; i++) {
+    const element = all_buttons[i]
+    if (element.className.includes('triangle')) {
+      colors_to_button['red'] = element
+    } else if (element.className.includes('diamond')) {
+      colors_to_button['blue'] = element
+    } else if (element.className.includes('circle')) {
+      colors_to_button['yellow'] = element
+    } else if (element.className.includes('square')) {
+      colors_to_button['green'] = element
+    }
+  }
+  return colors_to_button
+}
+
+function click(color) {
+  const iframe = document.getElementById('gameBlockIframe')
+  const inner_iframe = iframe.contentDocument || iframe.contentWindow.document
+  const colors_to_button = build_button_map(inner_iframe.getElementsByTagName('button'))
+  colors_to_button[color].click()
+}
+
+document.documentElement.addEventListener("keyup", function(e) {
+  if (window.location.href.includes('gameblock')) {
+    console.log(e)
+    console.log(document.getElementsByTagName('div'))
+    if (e.keyCode === 81) {
+      click('red')
+    } else if (e.keyCode === 87) {
+      click('blue')
+    } else if (e.keyCode === 69) {
+      click('yellow')
+    } else if (e.keyCode === 82) {
+      click('green')
+    }
+  }
+});
